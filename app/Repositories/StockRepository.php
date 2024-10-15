7b851cc2ce8da341a178d2f41c2e4ee3ea5a2989
imogore amhanesi joshua <32498529+josh3rill@users.noreply.github.com>
2024-10-15 18:36:35 +0200

    validatior added to StockRepository

--- app/Repositories/StockRepository.php
+++ app/Repositories/StockRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repositories;
 
 use App\Models\Stock;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class StockRepository implements StockRepositoryInterface
 {
@@ -13,11 +15,15 @@ class StockRepository implements StockRepositoryInterface
 
     public function getStockBySymbol($symbol)
     {
+        $this->validateSymbol($symbol);
         return Stock::where('symbol', $symbol)->first();
     }
 
     public function updateStock($symbol, $data)
     {
+        $this->validateSymbol($symbol);
+        $this->validateStockData($data);
+
         $stock = Stock::firstOrNew(['symbol' => $symbol, 'timestamp' => $data['timestamp']]);
         $stock->open = $data['open'];
         $stock->high = $data['high'];
@@ -26,5 +32,33 @@ class StockRepository implements StockRepositoryInterface
         $stock->volume = $data['volume'];
         $stock->previous_close = $data['previous_close'];
         $stock->save();
+    }
+
+    private function validateSymbol($symbol)
+    {
+        $validator = Validator::make(['symbol' => $symbol], [
+            'symbol' => 'required|string|max:10',
+        ]);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+    }
+
+    private function validateStockData($data)
+    {
+        $validator = Validator::make($data, [
+            'timestamp' => 'required|date',
+            'open' => 'required|numeric',
+            'high' => 'required|numeric',
+            'low' => 'required|numeric',
+            'close' => 'required|numeric',
+            'volume' => 'required|integer',
+            'previous_close' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
     }
 }
