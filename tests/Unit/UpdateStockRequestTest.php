4df4f2606d860711eadaf825aa8699a632d29fc3
imogore amhanesi joshua <32498529+josh3rill@users.noreply.github.com>
2024-10-16 08:52:29 +0200

    create updatestockrequesttest

---
+++ tests/Unit/UpdateStockRequestTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Requests\UpdateStockRequest;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+class UpdateStockRequestTest extends TestCase
+{
+    public function testValidationRules()
+    {
+        $request = new UpdateStockRequest;
+
+        $validator = Validator::make([
+            'timestamp' => now(),
+            'open' => 150,
+            'high' => 155,
+            'low' => 145,
+            'close' => 152,
+            'volume' => 1000,
+            'previous_close' => 148,
+        ], $request->rules());
+
+        $this->assertTrue($validator->passes());
+    }
+
+    public function testValidationFails()
+    {
+        $request = new UpdateStockRequest;
+
+        $validator = Validator::make([
+            'timestamp' => 'invalid-date',
+            'open' => 'invalid',
+            'high' => 'invalid',
+            'low' => 'invalid',
+            'close' => 'invalid',
+            'volume' => 'invalid',
+            'previous_close' => 'invalid',
+        ], $request->rules());
+
+        $this->assertFalse($validator->passes());
+    }
+}
