6df62a89eb3e103360d9e59bd7e0e5287adadd92
imogore amhanesi joshua <32498529+josh3rill@users.noreply.github.com>
2024-10-16 08:45:09 +0200

    update Fetchstockprice

--- app/Jobs/FetchStockPrice.php
+++ app/Jobs/FetchStockPrice.php
@@ -1,42 +1,42 @@
 <?php
 
-// namespace App\Jobs;
+namespace App\Jobs;
 
-// use App\Models\Stock;
-// use App\Services\AlphaVantageService;
-// use Illuminate\Bus\Queueable;
-// use Illuminate\Contracts\Queue\ShouldQueue;
-// use Illuminate\Foundation\Bus\Dispatchable;
-// use Illuminate\Queue\InteractsWithQueue;
-// use Illuminate\Queue\SerializesModels;
-// use Illuminate\Support\Facades\Log;
+use App\Models\Stock;
+use App\Services\AlphaVantageService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
-// class FetchStockPrice implements ShouldQueue
-// {
-//     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+class FetchStockPrice implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-//     protected $symbol;
+    protected $symbol;
 
-//     public function __construct($symbol)
-//     {
-//         $this->symbol = $symbol;
-//     }
+    public function __construct($symbol)
+    {
+        $this->symbol = $symbol;
+    }
 
-//     public function handle(AlphaVantageService $alphaVantageService)
-//     {
-//         try {
-//             $data = $alphaVantageService->getStockPrice($this->symbol);
-//             if ($data !== null) {
-//                 // Store data in the database
-//                 Stock::updateOrCreate(
-//                     ['symbol' => $this->symbol, 'timestamp' => $data['timestamp']],
-//                     $data
-//                 );
-//             } else {
-//                 Log::warning("No data returned for symbol: {$this->symbol}");
-//             }
-//         } catch (\Exception $e) {
-//             Log::error("Failed to fetch stock price for symbol: {$this->symbol}. Error: " . $e->getMessage());
-//         }
-//     }
-// }
+    public function handle(AlphaVantageService $alphaVantageService)
+    {
+        try {
+            $data = $alphaVantageService->getStockPrice($this->symbol);
+            if ($data !== null) {
+                // Store data in the database
+                Stock::updateOrCreate(
+                    ['symbol' => $this->symbol, 'timestamp' => $data['timestamp']],
+                    $data
+                );
+            } else {
+                Log::warning("No data returned for symbol: {$this->symbol}");
+            }
+        } catch (\Exception $e) {
+            Log::error("Failed to fetch stock price for symbol: {$this->symbol}. Error: " . $e->getMessage());
+        }
+    }
+}
