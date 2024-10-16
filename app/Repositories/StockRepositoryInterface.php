49912251ac2a1a0421bba4318d433a16b759a9e4
imogore amhanesi joshua <32498529+josh3rill@users.noreply.github.com>
2024-10-16 08:59:36 +0200

    update stockresponsitoryinterface

--- app/Repositories/StockRepositoryInterface.php
+++ app/Repositories/StockRepositoryInterface.php
@@ -5,6 +5,8 @@ namespace App\Repositories;
 interface StockRepositoryInterface
 {
     public function getAllStocks();
+
     public function getStockBySymbol($symbol);
+
     public function updateStock($symbol, $price);
 }
