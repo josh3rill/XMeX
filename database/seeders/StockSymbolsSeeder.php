171542a3594f9adef59e2e6761ed4fa2a7294412
imogore amhanesi joshua <32498529+josh3rill@users.noreply.github.com>
2024-10-16 08:57:02 +0200

    update  stocksymbolseeder

--- database/seeders/StockSymbolsSeeder.php
+++ database/seeders/StockSymbolsSeeder.php
@@ -2,10 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class StockSymbolsSeeder extends Seeder
 {
@@ -18,18 +17,18 @@ class StockSymbolsSeeder extends Seeder
     {
         // TODO: Implement the logic to FETCH $stockSymbols from database
         $stockSymbols = [
-            "AAPL", "GOOGL", "GOOG", "MSFT", "AMZN", "TSLA", "NVDA", "META", "JPM", "V"
+            'AAPL', 'GOOGL', 'GOOG', 'MSFT', 'AMZN', 'TSLA', 'NVDA', 'META', 'JPM', 'V',
         ];
 
         foreach ($stockSymbols as $symbol) {
             DB::table('stocks')->insert([
                 'symbol' => $symbol,
-                'open' => 567.000, // Default value, 
-                'high' => 567.000, // Default value, 
-                'low' => 567.000, // Default value, 
-                'close' => 319.000, // Default value, 
-                'previous_close' => 213.000, // Default value, 
-                'volume'	=> 1000000, // Default value,
+                'open' => 567.000, // Default value,
+                'high' => 567.000, // Default value,
+                'low' => 567.000, // Default value,
+                'close' => 319.000, // Default value,
+                'previous_close' => 213.000, // Default value,
+                'volume' => 1000000, // Default value,
                 'timestamp' => Carbon::now(),
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
