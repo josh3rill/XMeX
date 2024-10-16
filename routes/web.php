2148f5bbd5e96ef23d4bd722644fd33d244d33a6
imogore amhanesi joshua <32498529+josh3rill@users.noreply.github.com>
2024-10-16 08:55:36 +0200

    update web route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StockReportController;
+use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () { return view('welcome');});
 
- Route::get('/', [StockReportController::class, 'index']);
+Route::get('/', [StockReportController::class, 'index']);
