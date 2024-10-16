6590a49deb585ace48a025e41116dd11277e8283
imogore amhanesi joshua <32498529+josh3rill@users.noreply.github.com>
2024-10-16 03:36:27 +0200

    console

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,19 @@
 <?php
-
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\FetchStockPricesCommand;
+use App\Console\Commands\UpdateCacheCommand;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote')->everyFourMinutes();
+
+Artisan::command('update:cache', function () {
+    $command = new UpdateCacheCommand();
+    $command->handle();
+})->purpose('Update Cache From Database')->everyMinute();
+
+Artisan::command('fetch:stockprice', function () {
+    $command = new FetchStockPricesCommand(app(App\Services\AlphaVantageService::class));
+    $command->handle();
+})->purpose('Fetch Stock Price')->days('2');                                                                                                                                                                                                                                                                                                                                 
